5c2a66e153245ec7381131bbf6587de3a18a52b0
YonatanRA <yeho@mail.com>
2021-10-15 19:17:34 +0200

    repaso

---
+++ 02-Data_Analysis/func/funciones.py
@@ -0,0 +1,22 @@
+class Calculadora:
+    
+    def sumar(self, *args):
+        return sum(args)
+    
+    def restar(self, a, b):
+        return a-b
+    
+    def multi(self, a, b):
+        return a*b
+    
+    def divi(self, a, b):
+        return a/b
+    
+    
+    
+
+    
+class Mando2:
+    
+    def __init__(self):
+        self.color='negro'
